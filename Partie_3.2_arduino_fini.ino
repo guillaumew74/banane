aea2dbf539e43d31eaed655a3a548714ca2cebbd
ElcocoSensei <33550911+ElcocoSensei@users.noreply.github.com>
2017-11-15 16:03:12 +0100

    arduino allumage des LEDS

---
+++ Partie_3.2_arduino_fini.ino
@@ -0,0 +1,103 @@
+#include "param.h"
+#include "coeur.h"
+
+int pin11=11;
+int pin2=2;
+int pin3=3;
+int pin4=4;
+int pin5=5;
+int pin6=6;
+int pin7=7;
+int pin8=8;
+int pin9=9;
+int pin10=10;
+
+void setup() //initialisation de l'arduino
+
+{
+  // pinMode(Ledpin, OUTPUT) $
+  pinMode(pin11,OUTPUT); 
+  pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);
+  pinMode(pin4,OUTPUT);
+  pinMode(pin5,OUTPUT);
+  pinMode(pin6,OUTPUT);
+  pinMode(pin7,OUTPUT);
+  pinMode(pin8,OUTPUT);
+  pinMode(pin9,OUTPUT);
+  pinMode(pin10,OUTPUT);
+
+  Serial.begin(9600);  
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+
+ switch (choix) 
+ {
+    case 1:
+    {
+      allumer(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 2:
+    {
+      LedSur2_1(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 3:
+    {
+      LedSur2_2(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 4:
+    {
+      LedSur1_3(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 5:
+    {
+      LedSur1_4(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 6:
+    {
+      LedSur1_5(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 7:
+    {
+      LedSur1_6(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 8:
+    {
+      LedSur1_7(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 9:
+    {
+      LedSur1_8(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 10:
+    {
+      LedSur1_9(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 11:
+    {
+      LedSur1_10(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+    case 12:
+    {
+      chenille(pin11,  pin2,  pin3,  pin4,  pin5,  pin6,  pin7,  pin8,  pin9,  pin10, choix);
+      break;
+    }
+  }
+
+}
---
+++ coeur.h
@@ -0,0 +1,398 @@
+
+void allumer(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 1){
+    digitalWrite(pin11, HIGH);
+    digitalWrite(pin2, HIGH);
+    digitalWrite(pin3, HIGH);
+    digitalWrite(pin4, HIGH);
+    digitalWrite(pin5, HIGH);
+    digitalWrite(pin6, HIGH);
+    digitalWrite(pin7, HIGH);
+    digitalWrite(pin8, HIGH);
+    digitalWrite(pin9, HIGH);
+    digitalWrite(pin10, HIGH);
+  
+    delay(416);
+  
+    digitalWrite(pin11,LOW);
+    digitalWrite(pin2,LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6,LOW);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10,LOW);
+    delay(416);
+  }
+}
+
+void LedSur2_1(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 2){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,HIGH);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5,HIGH);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7,HIGH);
+    digitalWrite(pin8,HIGH);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9,HIGH);
+    digitalWrite(pin10, LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur2_2(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 3){
+    digitalWrite(pin11,LOW);
+    digitalWrite(pin2,HIGH );
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,HIGH );
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6,HIGH );
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,HIGH);
+    digitalWrite(pin8,LOW );
+    digitalWrite(pin9,HIGH);
+    digitalWrite(pin10,LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11,LOW);
+    digitalWrite(pin2,LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6,LOW);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10,LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_3(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 4){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4, HIGH);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7,HIGH);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9,HIGH);
+    digitalWrite(pin10, LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_4(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 5){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,HIGH);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8, HIGH);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10, LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_5(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 6){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6, HIGH);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10,HIGH);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_6(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 7){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7,HIGH);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10, LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_7(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 8){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,HIGH);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10, LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_8(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 9){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8, HIGH);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10, LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_9(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 10){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6,LOW);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin9,HIGH);
+    digitalWrite(pin10,LOW);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+void LedSur1_10(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 11){
+    digitalWrite(pin11,HIGH);
+    digitalWrite(pin2,LOW);
+    digitalWrite(pin3,LOW);
+    digitalWrite(pin4,LOW);
+    digitalWrite(pin5,LOW);
+    digitalWrite(pin6,LOW);
+    digitalWrite(pin7,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin8,LOW);
+    digitalWrite(pin9,LOW);
+    digitalWrite(pin10,HIGH);
+  
+    delay(416);
+  
+    digitalWrite(pin11, LOW);
+    digitalWrite(pin2, LOW);
+    digitalWrite(pin3, LOW);
+    digitalWrite(pin4, LOW);
+    digitalWrite(pin5, LOW);
+    digitalWrite(pin6, LOW);
+    digitalWrite(pin7, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin8, LOW);
+    digitalWrite(pin9, LOW);
+    digitalWrite(pin10, LOW);
+    delay(416);
+  }
+}
+
+
+void chenille(int pin11, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, int pin9, int pin10, int choix)
+{
+  while (choix == 12){
+    digitalWrite(pin11,HIGH);
+    delay(833);
+    digitalWrite(pin11,LOW);
+    delay(833);
+    digitalWrite(pin2, HIGH);
+    delay(833);
+    digitalWrite(pin2, LOW);
+    delay(833);
+    digitalWrite(pin3, HIGH);
+    delay(833);
+    digitalWrite(pin3, LOW);
+    delay(833);
+    digitalWrite(pin4, HIGH);
+    delay(833);
+    digitalWrite(pin4, LOW);
+    delay(833);
+    digitalWrite(pin5, HIGH);
+    delay(833);
+    digitalWrite(pin5, LOW);
+    delay(833);
+    digitalWrite(pin6, HIGH);
+    delay(833);
+    digitalWrite(pin6, LOW);
+    delay(833);
+    digitalWrite(pin7, HIGH);
+    delay(833);
+    digitalWrite(pin7, LOW);
+    delay(833);
+    digitalWrite(pin8, HIGH);
+    delay(833);
+    digitalWrite(pin8, LOW);
+    delay(833);
+    digitalWrite(pin9, HIGH);
+    delay(833);
+    digitalWrite(pin9, LOW);
+    delay(833);
+    digitalWrite(pin10, HIGH);
+    delay(833);
+    digitalWrite(pin10, LOW);
+  }
+}
+
+
